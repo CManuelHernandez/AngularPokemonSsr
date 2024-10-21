e88ebf5a3d8cc574269dfa79bf6189ade6fc96ff
C.Manuel Hernández <cmanuelhm87@gmail.com>
2024-10-21 13:14:08 +0200

    PLATFORM_ID - Cliente y Server

--- src/app/pages/pricing/pricing-page.component.ts
+++ src/app/pages/pricing/pricing-page.component.ts
@@ -18,17 +18,20 @@ import { Title, Meta } from '@angular/platform-browser';
 export default class PricingPageComponent implements OnInit {
   private title = inject(Title);
   private meta = inject(Meta);
+  private platform = inject(PLATFORM_ID);
 
   ngOnInit(): void {
+    // if (isPlatformBrowser(this.platform)) {
+    //   document.title = 'Pricing Page';
+    // }
+
+    // console.log({ hola: 'mundo' });
+
     this.title.setTitle('Pricing Page');
     this.meta.updateTag({
       name: 'description',
       content: 'Este es mi Pricing Page',
     });
     this.meta.updateTag({ name: 'og:title', content: 'Pricing Page' });
-    this.meta.updateTag({
-      name: 'keywords',
-      content: 'Hola,Mundo,Man H,Curso,Angular,PRO',
-    });
   }
 }
