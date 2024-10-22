36fa5bb1de97fead0275562ef12c6a65302d7c21
C.Manuel Hernández <cmanuelhm87@gmail.com>
2024-10-22 10:32:49 +0200

    Skeleton Loader

--- src/app/pages/pokemons/pokemons-page.component.ts
+++ src/app/pages/pokemons/pokemons-page.component.ts
@@ -1,12 +1,26 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  signal,
+} from '@angular/core';
 import { PokemonListComponent } from '../../pokemons/components/pokemon-list/pokemon-list.component';
+import { PokemonListSkeletonComponent } from './ui/pokemon-list-skeleton/pokemon-list-skeleton.component';
 
 @Component({
   selector: 'pokemons-page',
   standalone: true,
-  imports: [PokemonListComponent],
+  imports: [PokemonListComponent, PokemonListSkeletonComponent],
   templateUrl: './pokemons-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class PokemonsPageComponent {}
+export default class PokemonsPageComponent implements OnInit {
+  // public isLoading = signal(true);
+
+  ngOnInit(): void {
+    //   setTimeout(() => {
+    //     this.isLoading.set(false);
+    //   }, 5000);
+  }
+}
